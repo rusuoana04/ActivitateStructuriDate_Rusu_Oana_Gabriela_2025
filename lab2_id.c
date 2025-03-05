fdd47268d3d693a22ef61d107281034cf308f522
rusuoana22 <rusuoana22@stud.ase.ro>
2025-03-05 17:51:38 +0200

    creare si testare functie afisare

--- lab2_id.c
+++ lab2_id.c
@@ -12,17 +12,22 @@ struct Telefon {
 
 };
 
-struct Telefon initializare()
+struct Telefon initializare(int id, int ram, const char* producator,float pret, char serie)
 {
 	struct Telefon t;
-	t.id = 1;
-	t.RAM = 0;
+	t.id = id;
+	t.RAM = ram;
+	t.producator = (char*)malloc(sizeof(char) * (strlen(producator) + 1));
+	strcpy_s(t.producator, strlen(producator) + 1, producator);
+	t.pret = pret;
+	t.serie = serie;
+
 	return t;
 }
 
 void afisare(struct Telefon t)
 {
-
+	printf("% d. Telefonul %s , seria %c,  are %d Gb Ram, costa %5.2f Ron\n", t.id, t.producator, t.serie, t.RAM, t.pret);
 }
 
 void modifica_Atribut(struct Telefon t)
@@ -40,5 +45,9 @@ void dezalocare(struct Telefon* t)
 int main() {
 	
 	struct Telefon t;
+	//" " folosim pt sir caractere char* si '' pt un singur caracter
+	t = initializare(1, 256, "Samsung", 2000.5, 'A');
+
+	 afisare( t);
 	return 0;
 }
