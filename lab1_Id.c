8ac72db5585b1746477325edd3e85c5a4b47e710
rusuoana22 <rusuoana22@stud.ase.ro>
2025-03-05 16:45:08 +0200

    citirea unei variabile reale

--- lab1_Id.c
+++ lab1_Id.c
@@ -9,7 +9,13 @@ int main() {
   //trebuie folosit scanf_s
      scanf_s("%d", &variabila);
      printf("Ai introdus: %d", variabila);
-    
+     printf("Introduceti un nr real: ");
+     //citirea unei variabile reale
+ float variabila=0.0;
+ scanf_s("%f", &variabila);
+ //punem 5.2f ca sa ne apara numarul cu 2 zecimale
+  printf("Ai introdus: %5.2f", variabila);
+
 
     return 0;
 }
